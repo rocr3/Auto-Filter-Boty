1514e1cef9f1369a585554122898ee278069d077
Rakesh Halder <135441929+rocr3@users.noreply.github.com>
2024-02-03 18:57:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -81,16 +81,16 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/HA_Bots_Support')
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/HA_Bots')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/HA_Films_World')
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/HA_Bots")
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/HA_Bots")
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/AtWoner_bot')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/Actual_Thing')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/Actual_Thing')
+TUTORIAL = environ.get("TUTORIAL", " url='https://t.me/Notificationsactr/9")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", " url='https://t.me/Notificationsactr/9")
 
 # Bot settings
 DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-MAX_BTN = int(environ.get('MAX_BTN', 10))
+CACHE_TIME = int(environ.get('CACHE_TIME', 500))
+MAX_BTN = int(environ.get('MAX_BTN', 7))
 LANGUAGES = [language.lower() for language in environ.get('LANGUAGES', 'english hindi telugu tamil kannada malayalam').split()]
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 FILE_CAPTION = environ.get("FILE_CAPTION", script.FILE_CAPTION)
@@ -112,7 +112,7 @@ IMDB = is_enabled('IMDB', True)
 SPELL_CHECK = is_enabled("SPELL_CHECK", True)
 SHORTLINK = is_enabled('SHORTLINK', False)
 
-PAYMENT_QR = environ.get('PAYMENT_QR', 'http://graph.org/file/cacbbea472e5a48ce0d64.jpg')
+PAYMENT_QR = environ.get('PAYMENT_QR', 'http://graph.org/file/cacbea472e5a48ce0d64.jpg')
 
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', True)
